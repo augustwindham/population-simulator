2825efa7c78d5f35b9f2a443383872b8f528d3e8
augustwindham <augustwindham@gmail.com>
2020-03-26 18:14:04 -0500

    brains done: needs plot probably do in tkinter grid so it can a live plot

--- Population.py
+++ Population.py
@@ -1,24 +1,20 @@
 import random
-from matplotlib import pyplot as plt
 from math import sqrt
 import numpy as np
-
-area1 = (100,100)
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class Population(object):
     """docstring for Population."""
 
-
-
-    def __init__(self, size, hh_size, area, conectivity, p_infected, p_isolated, roi, p_die, loi, len_sim):
+    def __init__(self, size, area, conectivity, p_infected, p_isolated, p_die, loi, len_sim):
         super(Population, self).__init__()
         self.pop = size
-        self.hh_number = hh_size
         self.area = area
         self.conectivity = conectivity
         self.p_infected = p_infected
         self.p_isolated = p_isolated
-        self.roi = roi
+
         #self.p_recover = p_recover
         self.p_die = p_die
         self.len_i = loi
@@ -35,23 +31,23 @@ class Population(object):
         for i in range(self.pop):
             if i in inf and i in isolated:
                 self.date_i = 0
-                self.individuals.append(Person(i, True, True, movement[i], date_i = 0))
+                self.individuals.append(Person(i, True, True, movement[i], self.area, date_i = 0))
                 self.stops = movement[i]
 
             elif i in inf and i not in isolated:
                 self.date_i = 0
-                self.individuals.append(Person(i, True , False, movement[i], date_i = 0))
+                self.individuals.append(Person(i, True , False, movement[i], self.area, date_i = 0))
 
             elif i not in inf and i not in isolated:
-                self.individuals.append(Person(i, False, False, movement[i]))
+                self.individuals.append(Person(i, False, False, movement[i], self.area))
             else:
-                self.individuals.append(Person(i, False, True, movement[i]))
+                self.individuals.append(Person(i, False, True, movement[i], self.area))
         for obj in self.individuals:
-            obj.current_loc = [random.randint(0,area1[0]),random.randint(0,area1[1])]
-            #plot the objects
+            obj.current_loc = [random.randint(0,self.area[0]),random.randint(0,self.area[1])]
 
 
 
+        #individuall objects created. need to model daily individual motion in movement function
 
         for days in range(0,self.end_time):
             for hour in range(0,24):
@@ -61,6 +57,11 @@ class Population(object):
                     #print(p)
                     if p == 1:
                         obj.movment()
+                        obj.timer(days)
+                    else:
+                        if obj.infected ==True:
+                            obj.timer(days)
+                            #add a day to counter
 
             loc_list = []
 
@@ -68,55 +69,62 @@ class Population(object):
                 if obj.current_loc not in [x[1] for x in loc_list]:
                     loc_list.append([obj, obj.current_loc])
                 else:
-                    if obj.infected == False and loc_list[loc_list.index(obj.current_loc)][0].infected == False:
+                    if obj.infected == False and loc_list[[x[1]for x in loc_list].index(obj.current_loc)][0].infected == False:
                         pass
 
-                    elif obj.infected == False and loc_list[loc_list.index(obj.current_loc)][0].infected == True:
+                    elif obj.infected == False and loc_list[[x[1]for x in loc_list].index(obj.current_loc)][0].infected == True:
                         obj.infected = True
                         obj.date_i = days
-                    elif obj.infected == True and loc_list[loc_list.index(obj.current_loc)][0].infected == False:
-                        loc_list[loc_list.index(obj.current_loc)][0].infected = True
-                        loc_list[loc_list.index(obj.current_loc)][0].date_i = days
+                    elif obj.infected == True and loc_list[[x[1]for x in loc_list].index(obj.current_loc)][0].infected == False:
+                        loc_list[[x[1]for x in loc_list].index(obj.current_loc)][0].infected = True
+                        loc_list[[x[1]for x in loc_list].index(obj.current_loc)][0].date_i = days
                     else:
                         pass
-            print('day over')
-
-
+            num_i = 0
+            for obj in self.individuals:
+                if obj.infected:
+                    num_i+=1
+            print(num_i)
             #plot
 
 
         #dont forget to cure after 14 days
 
 
-    def update_infected_timers(self,obj):
-        pass
-
-        #individuall objects created. need to model daily individual motion in movement function
-
-
-
 
 class Person(object):
     """docstring for Person."""
     len = 1
-    def __init__(self, id,  infected, isolated, stops, date_i = ''):
+    def __init__(self, id,  infected, isolated, stops, area, date_i = '' ):
         super(Person, self).__init__()
+        self.area = area
+        self.isolated = isolated
         self.date_i = date_i
-        self.p_of_stop = stops/96
+        if self.isolated:
+            self.p_of_stop = .01
+        else:
+            self.p_of_stop = stops/96
         self.id = id
         self.infected = infected
         #self.hh_size = hh_size
-        self.isolated = isolated
+
         self.current_loc = ''
 
     def movment(self):
-        new_loc = [random.randint(0,area1[0]),random.randint(0,area1[1])]
+        new_loc = [random.randint(0,self.area[0]),random.randint(0,self.area[1])]
 
         while new_loc == self.current_loc:
-            new_loc = [random.randint(0,area1[0]),random.randint(0,area1[1])]
+            new_loc = [random.randint(0,self.area[0]),random.randint(0,self.area[1])]
 
         self.current_loc = new_loc
+    def timer(self,date):
+        if self.infected:
+            if date - self.date_i < 14:
+                pass
+            else:
+                self.infected == False
 
 
 
-Houston = Population(10, 4, (5,5), 5, .2, .70, .05, .02, 14, 7)
+
+Houston = Population(1000, (100,200), 5, .3, .90, .02, 14, 28)
